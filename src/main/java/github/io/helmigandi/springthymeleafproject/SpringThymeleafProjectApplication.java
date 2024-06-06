8ea58a84e28c48bef0540bf75c0887630f922070
helmigandi <sugandihelmi@gmail.com>
2024-06-06 16:29:49 +0700

    Initial commit

---
+++ src/main/java/github/io/helmigandi/springthymeleafproject/SpringThymeleafProjectApplication.java
@@ -0,0 +1,13 @@
+package github.io.helmigandi.springthymeleafproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringThymeleafProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringThymeleafProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/github/io/helmigandi/spring_thymeleaf_project/SpringThymeleafProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package github.io.helmigandi.spring_thymeleaf_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringThymeleafProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

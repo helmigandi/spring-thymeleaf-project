a3158114c07a6abda07e75382432e5ad5e9c9a10
helmigandi <sugandihelmi@gmail.com>
2024-06-10 11:09:26 +0700

    Add Chapter 7: controllers

--- src/main/java/github/io/helmigandi/springthymeleafproject/infrastructure/web/RootController.java
+++ src/main/java/github/io/helmigandi/springthymeleafproject/infrastructure/web/RootController.java
@@ -1,16 +1,15 @@
-package github.io.helmigandi.springthymeleafproject;
+package github.io.helmigandi.springthymeleafproject.infrastructure.web;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/")
-public class HomeController {
+@RequestMapping("/") // <.>
+public class RootController {
 
     @GetMapping
-    public String index(Model model) {
-        return "index";
+    public String root() {
+        return "redirect:/users"; // <.>
     }
 }
